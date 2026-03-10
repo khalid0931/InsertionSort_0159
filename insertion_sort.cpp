0699f10b5788a2a3fe238be0c0646b07918ed67b
khalid0931 <mutiarif53@gmail.com>
2026-03-10 10:31:05 +0700

    first commit

---
+++ insertion_sort.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int n;
+
+void input (){
+    while (true){
+        cout << "masukan jumlah data pada array = ";
+        cin >> n;
+
+        if (n <= 20){
+            break;
+        }
+        else{
+            cout << "\nArrat Tidak Boleh lebih dari 20\n";
+        }
+    }
+    cout << endl;
+    cout << "=============================================";
+    cout << "==============masukan elementv array=========";
+    cout << "=============================================";
+
+    for (int i = 0; i < n; i++){
+        cout << "data ke-" << (i+1) << ": ";
+        cin >> arr[i];
+    }
+}
+
+void insersitonsort(){
+    
+}
